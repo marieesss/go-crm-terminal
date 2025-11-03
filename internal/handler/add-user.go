d00a69e464bfcee4741b6fa0ea0746d65cb0f3d7
Marie <mje.espinosa@gmail.com>
2025-11-03 16:05:54 +0100

    style terminal

--- internal/handler/add-user.go
+++ internal/handler/add-user.go
@@ -12,32 +12,32 @@ import (
 func AddUser(contacts *[]domain.Contact) (domain.Contact, error) {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Nom de l'utilisateur : ")
+	fmt.Print(" 👀 Nom de l'utilisateur : ")
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSpace(name)
 
-	fmt.Print("Prénom de l'utilisateur : ")
+	fmt.Print(" 👀 Prénom de l'utilisateur : ")
 	surname, _ := reader.ReadString('\n')
 	surname = strings.TrimSpace(surname)
 
 	if name == "" || surname == "" {
-		return domain.Contact{}, fmt.Errorf("le nom et le prénom ne peuvent pas être vides")
+		return domain.Contact{}, fmt.Errorf("le nom et le prénom ne peuvent pas être vides 💀")
 	}
 
-	fmt.Print("Numéro de téléphone de l'utilisateur : ")
+	fmt.Print(" 👀 Numéro de téléphone de l'utilisateur : ")
 	phone, _ := reader.ReadString('\n')
 	phone = strings.TrimSpace(phone)
 
 	if phone == "" {
-		return domain.Contact{}, fmt.Errorf("le numéro de téléphone ne peut pas être vide")
+		return domain.Contact{}, fmt.Errorf("le numéro de téléphone ne peut pas être vide 💀")
 	}
 
-	fmt.Print("Email de l'utilisateur : ")
+	fmt.Print(" 👀 Email de l'utilisateur : ")
 	email, _ := reader.ReadString('\n')
 	email = strings.TrimSpace(email)
 
 	if email == "" {
-		return domain.Contact{}, fmt.Errorf("l'email ne peut pas être vide")
+		return domain.Contact{}, fmt.Errorf("l'email ne peut pas être vide 💀")
 	}
 
 	contact := domain.Contact{
